47e60e85dbe3adb0b316941b06c95a788bfecc56
Niilo Laitinen <niilo.laitinen@tuni.fi>
2021-12-01 11:34:25 +0200

    luo isObject

---
+++ test/isObject.test.js
@@ -0,0 +1,33 @@
+
+import chai from "chai"
+import isObject from "../src/isObject.js";
+const expect = chai.expect
+
+describe('the isObject function', () => {
+    it('should check if empty object is considered an object', () => {
+        const result = isObject({});
+        expect(result).to.be.eq(true);
+    });
+
+    it('should check if array is considered an object', () => {
+        const result = isObject([1, 2, 3]);
+        expect(result).to.be.eq(true);
+    });
+
+    it('should check if null is considered an object', () => {
+        const result = isObject(null);
+        expect(result).to.be.eq(false);
+    });
+
+    it('should check if a string is considered an object', () => {
+        const result = isObject(new String('abc'));
+        expect(result).to.be.eq(true);
+    });
+
+    it('should check if a string is considered an object', () => {
+        const result = isObject(true);
+        expect(result).to.be.eq(false);
+    });
+
+
+});
